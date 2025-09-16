39c317fa9f5910d241a6e5e0e979dd66aa6fcdb9
snowie2000 <mactype.feedback@gmail.com>
2025-09-16 17:04:15 +0800

    regex polyfill

--- packages/const/src/plugin.ts
+++ packages/const/src/plugin.ts
@@ -1,3 +1,5 @@
+import RegExp from 'regexp-polyfill';
+
 export const PLUGIN_SCHEMA_SEPARATOR = '____';
 export const PLUGIN_SCHEMA_API_MD5_PREFIX = 'MD5HASH_';
 
@@ -5,12 +7,20 @@ export const ARTIFACT_TAG = 'lobeArtifact';
 export const ARTIFACT_THINKING_TAG = 'lobeThinking';
 
 // https://regex101.com/r/TwzTkf/2
-export const ARTIFACT_TAG_REGEX = /<lobeArtifact\b[^>]*>(?<content>[\S\s]*?)(?:<\/lobeArtifact>|$)/;
+
+export const ARTIFACT_TAG_REGEX = new RegExp(
+  '<lobeArtifact\\b[^>]*>(?<content>[\\S\\s]*?)(?:<\\/lobeArtifact>|$)',
+);
+// /<lobeArtifact\b[^>]*>(?<content>[\S\s]*?)(?:<\/lobeArtifact>|$)/;
 
 // https://regex101.com/r/r9gqGg/1
-export const ARTIFACT_TAG_CLOSED_REGEX = /<lobeArtifact\b[^>]*>([\S\s]*?)<\/lobeArtifact>/;
+export const ARTIFACT_TAG_CLOSED_REGEX = new RegExp(
+  '<lobeArtifact\\b[^>]*>([\\S\\s]*?)<\\/lobeArtifact>',
+); // /<lobeArtifact\b[^>]*>([\S\s]*?)<\/lobeArtifact>/;
 
 // https://regex101.com/r/AvPA2g/1
-export const ARTIFACT_THINKING_TAG_REGEX = /<lobeThinking\b[^>]*>([\S\s]*?)(?:<\/lobeThinking>|$)/;
+export const ARTIFACT_THINKING_TAG_REGEX = new RegExp(
+  '<lobeThinking\\b[^>]*>([\\S\\s]*?)(?:<\\/lobeThinking>|$)',
+); // /<lobeThinking\b[^>]*>([\S\s]*?)(?:<\/lobeThinking>|$)/;
 
-export const THINKING_TAG_REGEX = /<think\b[^>]*>([\S\s]*?)(?:<\/think>|$)/;
+export const THINKING_TAG_REGEX = new RegExp('<think\\b[^>]*>([\\S\\s]*?)(?:<\\/think>|$)'); // /<think\b[^>]*>([\S\s]*?)(?:<\/think>|$)/;
